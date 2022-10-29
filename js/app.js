2c989be3a0876b1bdc9a7fcb1ac728b2229ad31a
Tiziano Montenegro <91438170+tizielpro@users.noreply.github.com>
2022-10-29 20:11:37 -0300

    added: dinamic articles, change themes and go up

--- js/app.js
+++ js/app.js
@@ -1,3 +1,66 @@
-$(document).ready(function(){
+$(document).ready(()=>{
+    // slider
     $(".slider").bxSlider()
+
+    // articles
+    let posts = [
+        {
+            title: "¡Hello World!",
+            date: "Posted on "+moment().format("MMM Do YYYY"),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, odit dolores minima quia sit quae error consequatur? Maxime dolores dignissimos odio nisi reprehenderit eligendi, facilis repudiandae ducimus ipsum? Fugit, dolores? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quasi fugit, vero sunt harum explicabo dolores molestias dicta numquam maiores eum repudiandae debitis odit veritatis. Blanditiis iure fugiat veniam magni.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quasi fugit, vero sunt harum explicabo dolores molestias dicta numquam maiores eum repudiandae debitis odit veritatis. Blanditiis iure fugiat veniam magni."
+        },
+        {
+            title: "¡Hello World!",
+            date: "Posted on "+moment().format("MMM Do YYYY"),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, odit dolores minima quia sit quae error consequatur? Maxime dolores dignissimos odio nisi reprehenderit eligendi, facilis repudiandae ducimus ipsum? Fugit, dolores? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quasi fugit, vero sunt harum explicabo dolores molestias dicta numquam maiores eum repudiandae debitis odit veritatis. Blanditiis iure fugiat veniam magni.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quasi fugit, vero sunt harum explicabo dolores molestias dicta numquam maiores eum repudiandae debitis odit veritatis. Blanditiis iure fugiat veniam magni."
+        },
+        {
+            title: "¡Hello World!",
+            date: "Posted on "+moment().format("MMM Do YYYY"),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, odit dolores minima quia sit quae error consequatur? Maxime dolores dignissimos odio nisi reprehenderit eligendi, facilis repudiandae ducimus ipsum? Fugit, dolores? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quasi fugit, vero sunt harum explicabo dolores molestias dicta numquam maiores eum repudiandae debitis odit veritatis. Blanditiis iure fugiat veniam magni.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quasi fugit, vero sunt harum explicabo dolores molestias dicta numquam maiores eum repudiandae debitis odit veritatis. Blanditiis iure fugiat veniam magni."
+        },
+        {
+            title: "¡Hello World!",
+            date: "Posted on "+moment().format("MMM Do YYYY"),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, odit dolores minima quia sit quae error consequatur? Maxime dolores dignissimos odio nisi reprehenderit eligendi, facilis repudiandae ducimus ipsum? Fugit, dolores? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quasi fugit, vero sunt harum explicabo dolores molestias dicta numquam maiores eum repudiandae debitis odit veritatis. Blanditiis iure fugiat veniam magni.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quasi fugit, vero sunt harum explicabo dolores molestias dicta numquam maiores eum repudiandae debitis odit veritatis. Blanditiis iure fugiat veniam magni."
+        },
+        {
+            title: "¡Hello World!",
+            date: "Posted on "+moment().format("MMM Do YYYY"),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, odit dolores minima quia sit quae error consequatur? Maxime dolores dignissimos odio nisi reprehenderit eligendi, facilis repudiandae ducimus ipsum? Fugit, dolores? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quasi fugit, vero sunt harum explicabo dolores molestias dicta numquam maiores eum repudiandae debitis odit veritatis. Blanditiis iure fugiat veniam magni.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quasi fugit, vero sunt harum explicabo dolores molestias dicta numquam maiores eum repudiandae debitis odit veritatis. Blanditiis iure fugiat veniam magni."
+        }
+    ];
+    posts.forEach((article, index)=>{
+        let post = `
+        <article class="section__article">
+            <div class="article--header">
+                <div class="article__left">
+                    <h2>${article.title}</h2>
+                    <p>${article.date}</p>
+                </div>
+                <button>📰 Comments</button>
+            </div>
+            <p class="paragraph">${article.content}</p>
+        </article>`;
+        $(".section").append(post);
+    });
+
+    // change theme
+    let theme = $("#theme");
+    $("#to-green").click(()=>{
+        theme.attr("href", "./css/green-theme.css")
+    });
+    $("#to-red").click(()=>{
+        theme.attr("href", "./css/red-theme.css")
+    });
+    $("#to-blue").click(()=>{
+        theme.attr("href", "./css/blue-theme.css")
+    });
+
+    //go up
+    $("#go-up").click((e)=>{
+        e.preventDefault();
+
+        $("html, body").animate({scrollTop: 0},400);
+    });
 });
