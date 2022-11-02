f42291e5e7e86ec70726488716d631601625d4fe
Tiziano Montenegro <91438170+tizielpro@users.noreply.github.com>
2022-11-02 19:57:57 -0300

    add form validation and update slider

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,25 @@
 $(document).ready(()=>{
     // validation
+    $("#contact--form").validate({
+        rules:{
+            name: {
+                required: true,
+                minlength: 2},
+            surname: {
+                required: true,
+                minlength: 3},
+            email: "required",
+            message: {
+                required: true,
+                minlength: 15}
+        },
+        messages: {
+            name: "please write a valid first name",
+            surname: "please write a valid last name",
+            email: "please write a valid email",
+            message: "please write a valid message"
+        }
+    })
     
     //go up
     $("#go-up").click((e)=>{
